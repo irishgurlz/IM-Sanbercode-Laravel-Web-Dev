d685afedf88471a3eee131c5a315af99ced204e0
irishgurlz <email andagit config --global user.email email>
2024-07-30 22:35:07 +0700

    delet hari-12

--- test-app-2/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\AuthController;
-
-Route::get('/', [HomeController::class, 'home']);
-Route::get('/register', [AuthController::class, 'register']);
-Route::post('/welcome', [AuthController::class, 'welcome']);
